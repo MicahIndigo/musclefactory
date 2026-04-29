37b446cf5a1a9fc475af1f6e9f6802c4f6d297c1
MicahIndigo <micah.music96@gmail.com>
2026-04-29 01:57:32 +0100

    Added GymClass and Booking models with admin setup

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class GymClass(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    gym_class = models.ForeignKey(GymClass, on_delete=models.CASCADE)
+    booed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.gym_class.name}"
