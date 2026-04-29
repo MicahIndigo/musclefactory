cbff4d2d3ea6152c5ddaa44daae2cb42ec9b8c0a
MicahIndigo <micah.music96@gmail.com>
2026-04-29 20:39:36 +0100

    Implemented class booking system with login protection

--- classes/urls.py
+++ classes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import class_list
+from .views import class_list, book_class
 
 
 urlpatterns = [
-    path('', class_list, name='class_list')
+    path('', class_list, name='class_list'),
+    path('book/<int:class_id>/', book_class, name='book_class'),
 ]
--- classes/views.py
+++ classes/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from .models import GymClass
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import GymClass, Booking
 
 
 def class_list(request):
     classes = GymClass.objects.all()
     return render(request, 'classes/class_list.html', {'classes': classes})
+
+
+@login_required
+def book_class(request, class_id):
+    gym_class = get_object_or_404(GymClass, id=class_id)
+
+    # prevent duplicate bookings
+    if Booking.objects.filter(user=request.user, gym_class=gym_class).exists():
+        return redirect('class_list')
+
+    Booking.objects.create(user=request.user, gym_class=gym_class)
+
+    return redirect('class_list')
