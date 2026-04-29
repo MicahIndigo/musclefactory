1b0ff504fe0723f51b9306af573b18ad2d97675c
MicahIndigo <micah.music96@gmail.com>
2026-04-29 22:29:37 +0100

    Added membership system and restricted booing classes to members

--- classes/views.py
+++ classes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import GymClass, Booking
+from profiles.models import Profile
 
 
 def class_list(request):
@@ -11,6 +12,12 @@ def class_list(request):
 @login_required
 def book_class(request, class_id):
     gym_class = get_object_or_404(GymClass, id=class_id)
+
+    profile = Profile.objects.get(user=request.user)
+
+    # Only members can book
+    if not profile.is_member:
+        return redirect('class_list')
 
     # prevent duplicate bookings
     if Booking.objects.filter(user=request.user, gym_class=gym_class).exists():
