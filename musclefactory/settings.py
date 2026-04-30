c8885f8d4e9060d7bace40f72fd174f256def6e4
MicahIndigo <micah.music96@gmail.com>
2026-04-30 21:38:30 +0100

    Configure static files for deployment

--- musclefactory/settings.py
+++ musclefactory/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://musclefactory.onrender.com"]
 
 
 # Application definition
